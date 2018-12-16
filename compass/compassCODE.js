d882a7d0f5c9282edfa30d0aef14b0d43686778d
Stikka <45919421+Stikka@users.noreply.github.com>
2018-12-16 20:29:59 +0100

    Create compassCODE.js

---
+++ compass/compassCODE.js
@@ -0,0 +1,15 @@
+basic.forever(() => {
+    let degrees = input.compassHeading();
+    if (degrees < 45 || degrees > 315) {
+        basic.showString("N");
+    }
+    else if (degrees < 135) {
+        basic.showString("E");
+    }
+    else if (degrees < 225) {
+        basic.showString("S");
+    }
+    else {
+        basic.showString("W");
+    }
+});
