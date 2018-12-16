61ab4b42a253dcb6243e14734a42c0991236c396
Stikka <45919421+Stikka@users.noreply.github.com>
2018-12-16 20:23:24 +0100

    Create rockpaperscissorsCODE.js

---
+++ rockpaperscissors/rockpaperscissorsCODE.js
@@ -0,0 +1,29 @@
+let hand = 0;
+input.onGesture(Gesture.Shake, () => {
+    hand = Math.randomRange(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
